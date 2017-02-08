f74ed0dd2a657f11497010c66d6bbfdb1819c1f8
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-02-08 13:40:58 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gem install yaml-lint
 yaml-lint topics.yml #Recomended: Put all the yml files in a folder such as yml so you can do yaml-lint yml so you do not have to go through all the trouble of adding each file one by one.
+sort_yaml < topics.yml > topics.sorted.yml && mv topics.sorted.yml topics.yml
