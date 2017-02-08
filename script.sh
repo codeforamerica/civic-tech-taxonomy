e8d4e278b209551ac8a4b3bea8feb007bed53c25
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-02-08 13:49:38 -0500

    Loops through files

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+FILES=*.yml
 gem install yaml-lint
-yaml-lint topics.yml #Recomended: Put all the yml files in a folder such as yml so you can do yaml-lint yml so you do not have to go through all the trouble of adding each file one by one.
-sort_yaml < topics.yml > topics.sorted.yml && mv topics.sorted.yml topics.yml
+for f in $FILES
+do
+  echo "Processing $f"
+  yaml-lint $f
+  sort_yaml < $f > $f && mv $f $f
+  echo "Done $f"
+done
