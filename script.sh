113e6a18f8f57409bb24f9d177543a0b15886924
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-03-22 16:06:16 -0400

    fix

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@
 set -e errexit
 set -o pipefail
 FILES=*.yml
-oldvalue=`cat $files`
 gem install yaml-lint
 gem install i18n_yaml_sorter
 for f in $FILES
 do
+  oldvalue=`cat $f`
   yaml-lint $f
   sort_yaml < $f > $f.tmp && mv $f.tmp $f
-  newvalue=`cat $files`
+  newvalue=`cat $f`
   if [ $newvalue == $oldvalue ]
   then 
     echo "Done"
