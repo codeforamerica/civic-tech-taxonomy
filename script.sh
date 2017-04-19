a501fb115df76c95a344b0f117d1c4395382b7a4
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-04-19 15:04:55 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,17 +5,20 @@ FILES=*.yml
 gem install yaml-lint
 gem install i18n_yaml_sorter
 for f in $FILES
-do
-  oldvalue=`cat $f`
-  yaml-lint $f
-  sort_yaml < $f > $f.tmp && mv $f.tmp $f
-  newvalue=`cat $f`
-  if [ $newvalue == $oldvalue ]
-  then 
-    echo "Done."
-    exit 0
-  else
-    echo "Build Failed."
-    exit 1
-  fi
-done
+  do
+    oldvalue=`cat $f`
+    yaml-lint $f
+    sort_yaml < $f > $f.tmp && mv $f.tmp $f
+    newvalue=`cat $f`
+    if [ $newvalue == $oldvalue ]
+    then 
+      echo "Done."
+      exit 0
+    else
+      echo "Build Failed."
+      exit 1
+    fi
+  done
+
+echo "Build Failed."
+exit 1 #For some reason Script did not work at all. What a shame.
