a5285629ef228b4e9ed4f2389ec424f9e9bd90ee
Chris Alfano <chris@jarv.us>
2021-07-23 20:03:12 +0000

    refactor: move taxonomies into common subdirectory

--- tools/mysql-loader/taxonomy-toml-2-mysql.py
+++ tools/mysql-loader/taxonomy-toml-2-mysql.py
@@ -24,11 +24,11 @@ ziprep = urlopen(zipurl)
 zfile = ZipFile(BytesIO(ziprep.read()))
 zfile.extractall('/tmp')
 
-logging.basicConfig(format='%(asctime)s - %(message)s', 
+logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[
                       logging.FileHandler("db_loader.log"),
                       logging.StreamHandler()
-                    ],					
+                    ],
                     level=logging.DEBUG)
 
 connection = MySQLdb.connect(host=db_host,
@@ -39,20 +39,20 @@ connection = MySQLdb.connect(host=db_host,
                              charset='utf8')
 
 logging.info("Connected to DB %s", db_db)
-							 
+
 cursor = connection.cursor()
 
 sql_get_org = "select id from organizations where name = %s"
 
-sql_save_tags = "INSERT INTO `taxonomy_tags` (`id`,`display_name`,`category`,`subcategory`) VALUES (%s, %s, %s, %s)" 
-sql_save_synonyms = "INSERT INTO `taxonomy_synonyms` (`tag_id`,`synonym`) VALUES (%s, %s)" 
+sql_save_tags = "INSERT INTO `taxonomy_tags` (`id`,`display_name`,`category`,`subcategory`) VALUES (%s, %s, %s, %s)"
+sql_save_synonyms = "INSERT INTO `taxonomy_synonyms` (`tag_id`,`synonym`) VALUES (%s, %s)"
 
 
 def load_data(dir):
   #logging.debug("Substring from %i", dir.rindex("/"))
   category = dir[dir.rindex("/")+1:]
   logging.info("Processing %s", category)
-  
+
   y = 0
 
   with os.scandir(dir) as files:
@@ -68,47 +68,47 @@ def load_data(dir):
         display_name = dict.get("display_name")
         synonyms = dict.get("synonyms")
         subcategory = dict.get("subcategory")
-		
+
         cursor.execute(sql_save_tags, (id, display_name, category, subcategory))
- 
+
         if synonyms == None:
           continue
-		
+
         for synonym in synonyms:
           cursor.execute(sql_save_synonyms, (id, synonym))
 
       except UnicodeEncodeError as ue_err:
         logging.exception("Error processing %s", fname)
-        continue            		  
+        continue
       except Exception as error:
         logging.exception("Error processing %s", fname)
         continue
-	
+
       logging.debug("Inserted tags and synonyms from %s", fname)
     return y
-	
+
 
 # only tags (statuses)
 # with subcategory and parent (technologies and skills)
 # with synonyms (issues)
 
 i = 0
-directory = '/tmp/civic-tech-taxonomy-master/issues'  #change to topics?
+directory = '/tmp/civic-tech-taxonomy-master/taxonomies/issues'  #change to topics?
 y = load_data(directory)
 logging.info("Processed %i files", y)
 i = i + y
 
-directory = '/tmp/civic-tech-taxonomy-master/skills'
+directory = '/tmp/civic-tech-taxonomy-master/taxonomies/skills'
 y = load_data(directory)
 logging.info("Processed %i files", y)
 i = i + y
 
-directory = '/tmp/civic-tech-taxonomy-master/technologies'
+directory = '/tmp/civic-tech-taxonomy-master/taxonomies/technologies'
 y = load_data(directory)
 logging.info("Processed %i files", y)
 i = i + y
 
-directory = '/tmp/civic-tech-taxonomy-master/statuses'
+directory = '/tmp/civic-tech-taxonomy-master/taxonomies/statuses'
 y = load_data(directory)
 logging.info("Processed %i files", y)
 i = i + y
